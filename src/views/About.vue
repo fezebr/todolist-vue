64bc9c64499bf740cc6b342342febf226b2338bd
faezeh ebrahimy <fezebr2000@gmail.com>
2025-02-25 12:20:08 +0330

    fix log

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>fezebr2000@gmail.com</div>
+  <div @click="sendTestEvent">fezebr2000@gmail.com</div>
 </template>
 
 <script setup>
@@ -8,6 +8,7 @@ import { useGtag } from "vue-gtag";
 const gtag = useGtag();
 
 const sendTestEvent = () => {
+  console.log("lo");
   gtag("event", "test_event", {
     event_category: "debug",
     event_label: "VueGtag test event",
