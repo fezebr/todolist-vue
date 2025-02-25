fbac3cd14adbba9c7c43841dfa79361af09523f8
faezeh ebrahimy <fezebr2000@gmail.com>
2025-02-25 12:18:39 +0330

    add test_event

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,3 +1,16 @@
 <template>
   <div>fezebr2000@gmail.com</div>
 </template>
+
+<script setup>
+import { useGtag } from "vue-gtag";
+
+const gtag = useGtag();
+
+const sendTestEvent = () => {
+  gtag("event", "test_event", {
+    event_category: "debug",
+    event_label: "VueGtag test event",
+  });
+};
+</script>
